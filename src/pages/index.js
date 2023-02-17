ea21a2600d45282346f16d4966be270577f6fedd
yochirolee <yleecruz@gmail.com>
2023-02-17 14:32:18 -0500

    some css 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,18 +54,7 @@ export default function Home() {
 										Poseemos un amplio catalogo de productos.
 									</p>
 									<div class="mt-8 flex gap-4 md:justify-center lg:justify-start">
-										<a
-											class="rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500"
-											href="/"
-										>
-											Get started
-										</a>
-										<a
-											class="rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400"
-											href="/"
-										>
-											View on GitHub
-										</a>
+										
 									</div>
 								</div>
 								<div class="relative lg:static xl:pl-10">
