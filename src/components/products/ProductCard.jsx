52e9a5a6506b4a979d2b65cf4ab422f5ef98743d
yochirolee <yleecruz@gmail.com>
2023-02-21 20:04:58 -0500

    react-form kkkasd as

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -5,21 +5,21 @@ import { React } from "react";
 export const ProductCard = ({ product }) => {
 	if (!product) return <div>No Products</div>;
 	return (
-		<Link href={`/catalogo/product/${product.slug}`} class="group">
-			<div class="aspect-w-1  aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+		<Link href={`/catalogo/product/${product.slug}`} className="group">
+			<div className="aspect-w-1  aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
 				<Image
 					src={product.image}
 					width={500}
 					height={500}
 					alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-					class="max-h-72 w-full object-top object-scale-down group-hover:opacity-75"
+					className="max-h-72 w-full object-top object-scale-down group-hover:opacity-75"
 				/>
 			</div>
-			<p class="mt-1 text-sm text-white rounded-lg p-2 text-center bg-medical ">
+			<p className="mt-1 text-sm text-white rounded-lg p-2 text-center bg-medical ">
 				{product.subCategories.name}
 			</p>
-			<h3 class="mt-4 text-lg font-medium text-gray-900 ">{product.name}</h3>
-			<p class="mt-1 text-sm text-gray-700 ">{product.categories.categoryName}</p>
+			<h3 className="mt-4 text-lg font-medium text-gray-900 ">{product.name}</h3>
+			<p className="mt-1 text-sm text-gray-700 ">{product.categories.categoryName}</p>
 		</Link>
 	);
 };
