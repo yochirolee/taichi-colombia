82d7ddb995fe1133f1a3e4a266c3648d680df8a5
yochirolee <yleecruz@gmail.com>
2023-02-23 12:51:22 -0500

    feautred products css

--- src/components/tables/ProductsTable.jsx
+++ src/components/tables/ProductsTable.jsx
@@ -1,26 +1,10 @@
 import { React, useEffect } from "react";
-import { BsSearch } from "react-icons/bs";
 import Image from "next/image";
 
 export const ProductsTable = ({ productsList }) => {
 	return (
 		<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-			<div className="flex items-center justify-between pb-4 bg-white ">
-				<label for="table-search" className="sr-only">
-					Buscar
-				</label>
-				<div className="relative">
-					<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-						<BsSearch className="text-gray-500" />
-					</div>
-					<input
-						type="text"
-						id="table-search-products"
-						className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
-						placeholder="Buscar Producto"
-					/>
-				</div>
-			</div>
+			
 			<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 				<thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
 					<tr>
--- src/pages/catalogo/newProduct.jsx
+++ src/pages/catalogo/newProduct.jsx
@@ -4,6 +4,7 @@ import { supabase } from "@/lib/supabaseClient";
 import CreateProductModal from "@/components/modals/createProductModal";
 import { ProductsTable } from "@/components/tables/ProductsTable";
 import { getAllProductsData } from "@/constants/products_Catalog";
+import { BsSearch } from "react-icons/bs";
 const NewProduct = ({ categories, subCategories, productsListData }) => {
 	const [open, setOpen] = useState(false);
 	const cancelButtonRef = useRef(null);
@@ -11,7 +12,7 @@ const NewProduct = ({ categories, subCategories, productsListData }) => {
 
 	return (
 		<Layout>
-			<div className="flex flex-col h-screen overflow-y-auto bg-white rounded-lg lg:p-6 gap-6">
+			<div className="flex flex-col h-screen overflow-y-auto p-2 bg-white lg:rounded-lg lg:p-6 gap-6">
 				<div className="flex justify-end gap-5">
 					<button
 						onClick={() => setOpen((prev) => !prev)}
@@ -21,6 +22,22 @@ const NewProduct = ({ categories, subCategories, productsListData }) => {
 					</button>
 					<button className="px-4 py-2 border rounded-lg text-sm">Crear Linea</button>
 					<button className="px-4 py-2 border rounded-lg text-sm">Crear Categoria</button>
+				</div>
+				<div className="flex items-center justify-between pb-4 bg-white ">
+					<label for="table-search" className="sr-only">
+						Buscar
+					</label>
+					<div className="relative">
+						<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+							<BsSearch className="text-gray-500" />
+						</div>
+						<input
+							type="text"
+							id="table-search-products"
+							className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+							placeholder="Buscar Producto"
+						/>
+					</div>
 				</div>
 				<ProductsTable productsList={productsList} />
 				<CreateProductModal
