f5edf83b7f6ede6d4941e22dcdf530f4c079c560
yochirolee <yleecruz@gmail.com>
2023-03-01 17:08:20 -0500

    icons

--- public/assets/index.js
+++ public/assets/index.js
@@ -7,5 +7,9 @@ import tokiobg from "./tokiobg.webp";
 import featuredbg from "./featuredbg.png";
 import chison from "./chison.png";
 import blurCyan from "./blur-cyan.png";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
 
-export { logo, people01, people02, people03, tokiobg, featuredbg, chison, blurCyan, mapa };
+export { logo, people01, people02, people03, tokiobg, featuredbg, chison, blurCyan, mapa,facebook,instagram,twitter,linkedin };
